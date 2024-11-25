38dc343b2af997a9b1a9221cf107d4e619834c0e
hectoramadorglez <hectoramador2005@gmail.com>
2024-11-25 14:13:59 +0100

    ghsj

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]private Transform pies;
+    [SerializeField]private float radio;
+    [SerializeField]private LayerMask layerMask;
+    [SerializeField] private int fuerzaMovimiento;
+    [SerializeField] private int fuerzaSalto;
+    [SerializeField] private Rigidbody2D rb;
+    private int hInput;
+    private Animator anim;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        // hInput = Input.GetAxisRaw("Horizontal");
+        if (hInput !=0) 
+        {
+            anim.SetBool("correr", true);
+        
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape) && Ensuelo() == true) 
+        {
+           rb.AddForce(new Vector2(0, 1) * fuerzaSalto, ForceMode2D.Impulse);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(hInput,1) * fuerzaMovimiento, ForceMode2D.Force);
+    }
+    private bool Ensuelo() 
+    {
+       Collider2D Resultado = Physics2D.OverlapCircle(pies.transform.position, radio,layerMask);
+
+        if (Resultado != null) 
+        {
+            return true;
+        
+        }
+        else 
+        { 
+            return false;
+        
+        }
+
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(pies.transform.position, radio);    
+    }
+}
